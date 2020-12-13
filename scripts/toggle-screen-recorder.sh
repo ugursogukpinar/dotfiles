07e2dbcce08c496e7c116d490055befc350b77aa
ugursogukpinar <sogukpinar.ugur@gmail.com>
2020-12-13 12:28:21 +0300

    no need for extra sway command in script

--- scripts/toggle-screen-recorder.sh
+++ scripts/toggle-screen-recorder.sh
@@ -5,7 +5,7 @@ status=$?
 
 if [ $status != 0 ] 
 then 
-  swaymsg exec -- wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
+  wf-recorder -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
 else 
-  swaymsg exec -- pkill --signal SIGINT wf-recorder
+  pkill --signal SIGINT wf-recorder
 fi;
